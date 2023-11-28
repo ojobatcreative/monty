a8e0b4332be0060ae4431681f79fd893d95f56ee
Babatunde <babatundeojomu2016@gmail.com>
2023-11-29 00:07:42 +0100

    main file

--- main.c
+++ main.c
@@ -1,14 +1,11 @@
 #include "monty.h"
-
 bus_t bus = {NULL, NULL, NULL, 0};
-
 /**
-* main - interprets the monty code
+* main - monty code interpreter
 * @argc: number of arguments
-* @argv: monty file locations
+* @argv: monty file location
 * Return: 0 on success
 */
-
 int main(int argc, char *argv[])
 {
 	char *content;
